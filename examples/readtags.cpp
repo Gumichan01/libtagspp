45801b0fcffc66d6fa61102c6534be218d4c6e06
Gumichan01 <luxon.jean.pierre@gmail.com>
2016-08-23 15:41:00 +0200

    readtags.cpp: update

--- examples/readtags.cpp
+++ examples/readtags.cpp
@@ -1,96 +1,39 @@
 
-#include <tags.h>
-#include <stdio.h>
+#include <libtagpp.hpp>
+#include <iostream>
 
-typedef struct Aux Aux;
+using namespace std;
 
-struct Aux
+
+int main(int argc, char **argv)
 {
-	FILE * fd;
-};
 
-static const char *t2s[] =
-{
-	"Artist",
-	"Album",
-	"Title",
-	"Date",
-	"Track",
-	"Albumgain",
-	"Albumpeak",
-	"Trackgain",
-	"Trackpeak",
-	"Genre",
-	"Image",
-};
-
-static void
-cb(Tagctx *ctx, int t, const char *v, int offset, int size, Tagread f)
-{
-	if(t == Timage)
-		printf("%-12s %s %d %d\n", t2s[t], v, offset, size);
-	else
-		printf("%-12s %s\n", t2s[t], v);
-}
-
-static int
-ctxread(Tagctx *ctx, void *buf, int cnt)
-{
-	Aux *aux = (Aux *) ctx->aux;
-	return fread(buf,1,cnt,aux->fd);
-}
-
-static int
-ctxseek(Tagctx *ctx, int offset, int whence)
-{
-	Aux *aux = (Aux *) ctx->aux;
-    fseek(aux->fd, offset, whence);
-	return ftell(aux->fd);
-}
-
-int
-main(int argc, char **argv)
-{
-	int i;
-	char buf[256];
-	Aux aux;
-	Tagctx ctx =
-	{
-        NULL,
-		ctxread,
-		ctxseek,
-		cb,
-        &aux,
-		buf,
-		sizeof(buf)
-	};
-
-	if(argc < 2){
-		printf("usage: readtags FILE...\n");
+	if(argc < 2)
+    {
+		cout << "usage: readtags FILE..." << endl;;
 		return -1;
 	}
 
-	for(i = 1; i < argc; i++){
-		printf("*** %s\n", argv[i]);
-		if((aux.fd = fopen(argv[i], "rb")) < 0)
-			printf("failed to open\n");
-		else{
-			ctx.filename = argv[i];
-			if(tagsget(&ctx) != 0)
-				printf("no tags or failed to read tags\n");
-			else{
-				if(ctx.duration > 0)
-					printf("%-12s %d ms\n", "duration", ctx.duration);
-				if(ctx.samplerate > 0)
-					printf("%-12s %d\n", "samplerate", ctx.samplerate);
-				if(ctx.channels > 0)
-					printf("%-12s %d\n", "channels", ctx.channels);
-				if(ctx.bitrate > 0)
-					printf("%-12s %d\n", "bitrate", ctx.bitrate);
-			}
-			fclose(aux.fd);
-		}
-		printf("\n");
+	for(int i = 1; i < argc; i++)
+    {
+        cout << "*** " << argv[i] << endl;;
+
+        libtagpp::Tag tag;
+        if(tag.readTag(argv[i]))
+        {
+            cout << "Title - " << tag.title() << endl
+                 << "Artist - " << tag.artist() << endl
+                 << "Album - " << tag.album() << endl
+                 << "Year - " << tag.year() << endl
+                 << "Track - " << tag.track() << endl
+                 << "Genre - " << tag.genre() << endl
+                 << " ===============================" << endl
+                 << "Duration - " << tag.properties().duration() << endl;
+        }
+        else
+            cerr << "Cannot read the tag of the following file: " << endl
+                 << "--- " << argv[1] << endl;
+
 	}
 	return 0;
 }
